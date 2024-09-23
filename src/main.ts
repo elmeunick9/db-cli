ff0b28bcb0c6db800123093e2efb63d4e2768a70
Robert Planas <robert@hubbit86.com>
2024-09-24 01:50:29 +0200

    Fix

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,9 @@ program.command('generate')
     .argument('[version]', 'Version to use', config.isDevelopment() ? 'next' : 'latest')
     .option('-j, --json <file>', 'JSON schema file', 'generated/db-schema.json')
     .option('-t, --ts <file>', 'TS file', 'generated/db-cli.ts')
-    .action(async (v) => await db.generate(v, program.opts().json, program.opts().ts))
+    .action(async (v, options) => {
+        await db.generate(v, options.json, options.ts)
+    })
 
 program.parse()
 
